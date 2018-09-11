e5039325b03037a09458514ca2a208f6311e108f
Jonas <admin@j-kit.de>
2018-09-11 17:50:40 +0200

    Add project files.

---
+++ AP.WordParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP.WordParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
